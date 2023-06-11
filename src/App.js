bdd5c4ec90664b7e76c82ab66312864190f46962
dinaAlsaid <41339383+dinaAlsaid@users.noreply.github.com>
2023-06-11 21:13:46 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import moment from 'moment';
+
+
+const dayStart = moment({ hours: 8, minutes: 0 })
+const dayEnd = moment({ hours: 17, minutes: 30 })
+const nineOclk = dayStart.clone().add(1, 'hours');
+const fourOclk = dayEnd.clone().subtract(1, "hours");
+
+const OutTimeForm = () => {
+  const [inTime, setInTime] = useState(dayStart)
+  const [outTime, setOutTime] = useState(dayEnd)
+
+  useEffect(() => {
+
+    if (inTime.isBetween(dayStart, nineOclk)) {
+      setOutTime(inTime.clone().add(8, 'hours').add(30, "minutes"))
+    } else if (inTime.isSameOrBefore(dayStart)) {
+      setOutTime(fourOclk)//4:30
+    } else if (inTime.isSameOrAfter(nineOclk)) {
+      setOutTime(dayEnd)//5:30
+    }
+
+  }, [inTime])
+
+  const onChangeIn = (e) => {
+    setInTime(moment(e.target.value, "HH:mm"))
+  }
+  return (<>
+    <div>
+      <input type="time" value={inTime.format('HH:mm')} onChange={onChangeIn} />
+    </div>
+
+    <div>
+      {/* <input type="time" value={outTime.format('HH:mm')} onChange={onChangeOut} /> */}
+      OUT TIME:
+      {outTime.format('HH:mm')}
+    </div>
+  </>)
+}
+
+const LeaveCalcForm = () => {
+  const [inTime, setInTime] = useState(dayStart)
+  const [outTime, setOutTime] = useState(dayEnd)
+
+  useEffect(() => {
+
+    if (inTime.isBetween(dayStart, nineOclk)) {
+      setOutTime(inTime.clone().add(8, 'hours').add(30, "minutes"))
+    } else if (inTime.isSameOrBefore(dayStart)) {
+      setOutTime(fourOclk)//4:30
+    } else if (inTime.isSameOrAfter(nineOclk)) {
+      setOutTime(dayEnd)//5:30
+    }
+
+  }, [inTime])
+
+  const onChangeIn = (e) => {
+    setInTime(moment(e.target.value, "HH:mm"))
+  }
+  return (<>
+    <div>
+      <input type="time" value={inTime.format('HH:mm')} onChange={onChangeIn} />
+    </div>
+
+    <div>
+      {/* <input type="time" value={outTime.format('HH:mm')} onChange={onChangeOut} /> */}
+      OUT TIME:
+      {outTime.format('HH:mm')}
+    </div>
+  </>)
+}
+
 
 function App() {
+  const [mode, setMode] = useState(0)
+  const changeCalcEq = (e) => {
+    setMode(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
       </header>
+
+      <select onChange={changeCalcEq} value={mode} >
+        <option value={0}>حساب وقت الخروج</option>
+        <option value={1}>حساب مدة المغادرة</option>
+      </select>
+
+      {!mode && (<OutTimeForm />)}
+      {!!mode && (<LeaveCalcForm />)}
+
+
     </div>
   );
 }
